ffca1b844809da7ed2631427bf131600c6aa3378
Justin Faulkenberry <justin.faulkenberry@gmail.com>
2021-05-04 13:58:54 -0500

    nav changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Skills from "./components/Skills"
 import Portfolio from "./components/Portfolio";
-import "./App.css";
 import Contact from "./components/Contact";
 import TopNav from "./components/TopNav";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       <div className="App">
         <TopNav />
         <Route exact path="/" component={Home} />
+        <Route path="/Skills" component={Skills} />
         <Route path="/Portfolio" component={Portfolio} />
         <Route path="/Contact" component={Contact} />
       </div>
